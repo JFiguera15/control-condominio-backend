75b3c7b5faaf1e679311dedba239bcf1f04b9097
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-06-28 15:31:32 -0400

    Login middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,15 @@
 import { prisma } from '../config/config.js'
 import { exclude } from '../middleware/GeneralMiddleware.js'
 import bcrypt from 'bcrypt'
-import jwt from "jsonwebtoken"
+import jwt from 'jsonwebtoken'
 
 export async function getAllUsers(req, res) {
 	const users = await prisma.user.findMany()
 
 	if (users.length <= 0) {
-		res.status(404).json({
+		return res.status(404).json({
 			message: 'No users found',
 		})
-		return
 	}
 
 	const filteredUsers = users.map((user) => exclude(user, ['password']))
@@ -19,11 +18,11 @@ export async function getAllUsers(req, res) {
 }
 
 export async function addUser(req, res) {
+	const salt = bcrypt.genSaltSync(process.env.SALT_ROUNDS)
+	const hashedPassword = bcrypt.hashSync(req.body.password, salt)
 	// Remove confirmPassword field from body
 	const cleanUserInfo = exclude(req.body, ['confirmPassword'])
-	const salt = bcrypt.genSaltSync(process.env.SALT_ROUNDS)
-	const newPass = bcrypt.hashSync(req.body.password, salt)
-	const hashedUser = { ...cleanUserInfo, password: newPass }
+	const hashedUser = { ...cleanUserInfo, password: hashedPassword }
 	const newUser = await prisma.user.create({
 		data: hashedUser,
 	})
@@ -46,14 +45,23 @@ export async function login(req, res) {
 	try {
 		const user = await prisma.user.findFirst({
 			where: {
-				email: email
-			}
+				email: email,
+			},
 		})
-		if (!user) console.log("no existe")
+
 		const validPass = bcrypt.compareSync(password, user.password)
-		if (!validPass) console.log("contraseña incorrecta")
-		const token = jwt.sign({ id: user.id }, process.env.SECRET_JWT_KEY, { expiresIn: "1d" })
-		res.status(200).cookie("access_token", token, { httpOnly: true }).json({ token: token })
+		if (!validPass) {
+			return res.status(400).json({
+				message: 'Invalid password',
+			})
+		}
+		const token = jwt.sign({ id: user.id }, process.env.SECRET_JWT_KEY, {
+			expiresIn: '1d',
+		})
+		res
+			.status(200)
+			.cookie('access_token', token, { httpOnly: true })
+			.json({ token: token })
 	} catch (error) {
 		res.status(401).send(error.message)
 	}
--- middleware/userMiddleware.js
+++ middleware/userMiddleware.js
@@ -6,11 +6,32 @@ export const validateFields = async (req, res, next) => {
 		const result = userSignupSchema.safeParse(req.body)
 
 		if (!result.success) {
-			res.status(400).json(result.error)
-			return
+			return res.status(400).json(result.error)
 		}
 
 		next()
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ message: 'Something is wrong, try again later' })
+	}
+}
+
+export const userDoesNotExist = async (req, res, next) => {
+	try {
+		const { email } = req.body
+		const user = await prisma.user.findFirst({
+			where: {
+				email: email,
+			},
+		})
+
+		if (!user) {
+			return next()
+		}
+
+		res.status(400).json({
+			message: 'User already exist',
+		})
 	} catch (error) {
 		console.log(error)
 		res.status(500).json({ message: 'Something is wrong, try again later' })
@@ -26,13 +47,12 @@ export const userExists = async (req, res, next) => {
 			},
 		})
 
-		if (!user) {
-			next()
-			return
+		if (user) {
+			return next()
 		}
 
 		res.status(400).json({
-			message: 'User already exist',
+			message: 'User does not exist',
 		})
 	} catch (error) {
 		console.log(error)
