5e9a158a0bee74958ffbf6c323d3a2935910b6e2
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-06-28 15:32:44 -0400

    House middleware

---
+++ middleware/houseMiddelware.js
@@ -0,0 +1,23 @@
+import { prisma } from '../config/config'
+
+export const houseExistsForPayment = async (req, res, next) => {
+	try {
+		const { houseId } = req.body
+		const house = await prisma.house.findFirst({
+			where: {
+				id: houseId,
+			},
+		})
+
+		if (house) {
+			return next()
+		}
+
+		res.status(400).json({
+			message: 'House does not exist',
+		})
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ message: 'Something is wrong, try again later' })
+	}
+}
