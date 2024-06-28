e5ac97d2a0f2f6fec0bd4c96c57eb8f05d661278
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-06-28 15:32:14 -0400

    Add user middlewares

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,21 @@
 import { Router } from 'express'
-import { addUser, getAllUsers, getUser, login } from '../controllers/userController.js'
-import { userExists, validateFields } from '../middleware/userMiddleware.js'
+import {
+	addUser,
+	getAllUsers,
+	getUser,
+	login,
+} from '../controllers/userController.js'
+import {
+	userDoesNotExist,
+	userExists,
+	validateFields,
+} from '../middleware/userMiddleware.js'
 
 const router = Router()
 
 router.route('/').get(getAllUsers)
 router.route('/:id').get(getUser)
-router.route('/').post(userExists, validateFields, addUser)
-router.route('/token').post(login)
+router.route('/').post(userDoesNotExist, validateFields, addUser)
+router.route('/token').post(userExists, login)
 
 export default router
