d15e0c6359c233cf008da0f5200f0b70831d2a6c
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-06-28 15:32:54 -0400

    Payment middleware

---
+++ middleware/paymentMiddleware.js
@@ -0,0 +1,21 @@
+import { prisma } from '../config/config.js'
+
+export const refExists = async (req, res, next) => {
+	try {
+		const ref = prisma.payment.findUnique({
+			where: {
+				ref: req.body.ref,
+			},
+		})
+		if (!ref) {
+			return next()
+		}
+
+		res.status(400).json({
+			message: 'Ref already exists',
+		})
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ message: 'Something is wrong, try again later' })
+	}
+}
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,12 +1,19 @@
-import { Router } from "express";
-import { addPayment, getHousePayments, getUserPayments } from "../controllers/paymentController.js";
-import { verifyToken } from "../middleware/GeneralMiddleware.js";
+import { Router } from 'express'
+import {
+	addPayment,
+	getHousePayments,
+	getUserPayments,
+} from '../controllers/paymentController.js'
+import { verifyToken } from '../middleware/GeneralMiddleware.js'
+import { refExists } from '../middleware/paymentMiddleware.js'
+import { houseExistsForPayment } from '../middleware/houseMiddelware.js'
 
-const router = Router();
+const router = Router()
 
-router.route("/").post(verifyToken, addPayment)
-router.route("/house/:id").get(verifyToken, getHousePayments)
-router.route("/user").get(verifyToken, getUserPayments)
+router.route('/').post(verifyToken, refExists, addPayment)
+router
+	.route('/house/:id')
+	.get(verifyToken, houseExistsForPayment, getHousePayments)
+router.route('/user').get(verifyToken, getUserPayments)
 
 export default router
-
