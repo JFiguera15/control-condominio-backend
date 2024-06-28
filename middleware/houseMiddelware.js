4d086d6d5d0b46675a48d9282a25f0784a4668ae
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-06-28 15:41:19 -0400

    House middleware

--- middleware/houseMiddelware.js
+++ middleware/houseMiddelware.js
@@ -21,3 +21,24 @@ export const houseExistsForPayment = async (req, res, next) => {
 		res.status(500).json({ message: 'Something is wrong, try again later' })
 	}
 }
+
+export const houseExists = async (req, res, next) => {
+	try {
+		const house = await prisma.house.findFirst({
+			where: {
+				id: req.params.id,
+			},
+		})
+
+		if (house) {
+			return next()
+		}
+
+		res.status(400).json({
+			message: 'House does not exist',
+		})
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ message: 'Something is wrong, try again later' })
+	}
+}
--- routes/houseRoutes.js
+++ routes/houseRoutes.js
@@ -1,13 +1,16 @@
-import { Router } from "express";
-import { addHouse, getHouseById, getHousesFromUser } from "../controllers/houseController.js";
-import { verifyToken } from "../middleware/GeneralMiddleware.js";
+import { Router } from 'express'
+import {
+	addHouse,
+	getHouseById,
+	getHousesFromUser,
+} from '../controllers/houseController.js'
+import { verifyToken } from '../middleware/GeneralMiddleware.js'
+import { houseExists } from '../middleware/houseMiddelware.js'
 
-const router = Router();
+const router = Router()
 
-router.route("/").get(verifyToken, getHousesFromUser)
-router.route("/:id").get(verifyToken, getHouseById)
-router.route("/").post(verifyToken, addHouse)
+router.route('/').get(verifyToken, getHousesFromUser)
+router.route('/:id').get(verifyToken, houseExists, getHouseById)
+router.route('/').post(verifyToken, addHouse)
 
-export default router;
-
-
+export default router
